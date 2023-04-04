04958eace63e67bba735c11dea5a08b4788b9499
Mahery Gonin <mahery.gonin@gmail.com>
2023-04-04 05:02:35 +0200

    Init project / Create routes

---
+++ src/pages/About.jsx
@@ -0,0 +1,25 @@
+import React from "react";
+
+const About = () => {
+  return (
+    <div>
+      <h1>About</h1>
+      <p>
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Vitae
+        voluptates quod, distinctio ipsa quae debitis ex quam. Quia, labore!
+        Molestiae vitae distinctio recusandae aliquam architecto consequuntur
+        hic quibusdam? Voluptatibus aperiam provident cum veritatis. Quisquam
+        temporibus repudiandae est corporis facere id porro, quidem ullam aut?
+        Fugiat, distinctio esse odit sint hic voluptate facere sed nisi
+        repellendus voluptatibus optio reprehenderit molestias. Alias ullam iure
+        eaque at fuga nisi quae esse, cupiditate laborum asperiores quos
+        exercitationem minima aspernatur. Non quidem quam animi mollitia, aut
+        est molestias ab neque id earum exercitationem aspernatur recusandae
+        enim, delectus omnis qui. Quibusdam sequi facilis officia numquam
+        laboriosam.
+      </p>
+    </div>
+  );
+};
+
+export default About;
